d16df41ec43beff429e09ec8144344d9a0586b0c
Justin Cheong <justin_cheong@tech.gov.sg>
2020-04-17 15:16:12 +0800

    updated with default reducer

---
+++ src/rdcPersonalDetails.js
@@ -0,0 +1,11 @@
+const initialState = {
+    contact: '',
+};
+  
+const rdcPersonalDetails = (state=initialState, action) => {
+    console.log(action);
+    return state;
+};
+
+export default rdcPersonalDetails;
+  
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,4 +1,6 @@
 import {combineReducers} from 'redux';
+import personalDetails from './rdcPersonalDetails';
 
 export default combineReducers({
+    personalDetails,
 });
